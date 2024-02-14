de58ca230316f7e4a92f9188233bd9851c4ff464
Gillian Minnehan <gillian@memfault.com>
2024-02-14 11:59:54 -0500

    Add support for build id and coredump partition

---
+++ add_build_id.py
@@ -0,0 +1,19 @@
+"""
+Small wrapper to hook the Memfault build id script into platform IO.
+"""
+
+Import("env")
+
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(
+        " ".join(
+            [
+                "$PYTHONEXE",
+                "$PROJECT_DIR/src/memfault/memfault-firmware-sdk/scripts/fw_build_id.py",
+                "$BUILD_DIR/${PROGNAME}.elf",
+            ]
+        ),
+        "Adding build id to $BUILD_DIR/${PROGNAME}.elf",
+    ),
+)
